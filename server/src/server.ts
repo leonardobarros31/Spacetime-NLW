efbcccca8f70a22b014e846a584a34719b45cddf
Leonardo Barros <leonardobarros9931@gmail.com>
2023-05-19 17:00:36 -0300

    aula 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -8,6 +12,11 @@ app.register(cors, {
   origin: 'true', // todas as URLs de front-end podem acessar o back-end
 })
 
+app.register(jwt, {
+  secret: 'cun1093y502189nty19t9p0yt5r1p10c7n9151275091',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
